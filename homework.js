546872c3763df5e631e9718112d523755f12a2af
Crz9410 <110355965+Crz9410@users.noreply.github.com>
2022-09-15 19:47:55 -0500

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,33 +1,113 @@
-'use strict'
+"use strict";
 
-function BinarioADecimal(num) {
-  // tu codigo aca
-  // declaramos la variable decimal vacia en 0, declaramos la esctrutura for para iterar todos los elementos que hayan en (num) con .length y la variable i vaya pasando al otro elemento
-  // decimal es igual a num[i]por dos y se eleva **(num.length-1 -i) en el .length ponemos -1 y -i, para que nos de el reverso. retornamos let.
+// Closures
+
+function counter() {
+  /*
+  Ejercicio 1
+
+  La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
+
+  Ejemplo:
+  const nuevoContador = counter()
+  nuevoContador()     // 1
+  nuevoContador()     // 2
+  nuevoContador()     // 3
+
+  const otroContador = counter()
+  otroContador()      // 1
+  otroContador()      // 2
+  otroContador()      // 3
+   */
+  var contador = 0;
+  return function () {
+    return ++contador;
+  }
+}
+
+function cacheFunction(cb) {
+
+  /*
+  Ejercicio 2
+
+  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+
+  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+
+
+  Ejemplo:
+  function square(n){
+    return n * n
+  }
+
+  const squareCache = cacheFunction(square)
+
+  squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
+  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
+
+  */
+  var cache = {};
+  return function (arg) {
+    if (cache.hasOwnProperty(arg)) {
+      return cache[arg];
+    } else {
+      var newProp = cb(arg);
+      cache[arg] = newProp;
+      return newProp;
+    }
+  }
+}
+
+// Bind
+
+var instructor = {
+  nombre: "Franco",
+  edad: 25,
+};
+
+var alumno = {
+  nombre: "Juan",
+  curso: "FullStack",
+};
+
+function getNombre() {
+  return this.nombre;
+}
+
+/*
+  Ejercicio 3
+
+  IMPORTANTE: no modificar el código de arriba (variables instructor y alumno, y función getNombre)
+
+  Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
+*/
+
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
+
+/*
+  Ejercicio 4
   
-  let decimal = 0;
-  for (let i = 0; i < num.length; i++){
-    decimal += +num[i] *2 **(num.length-1 -i);
-    
-  }
-  return decimal;
- 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+*/
+
+function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
+  return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-function DecimalABinario(num) {
-  // tu codigo aca
-  let binario =[];
-  while (num>0) {
-    let cociente = Math.floor(num/2);
-    let resto = num%2;
-    binario.unshift(resto);
-    num = cociente;
-  }
-  return binario.join("");
-}
+let textoAsteriscos = crearCadena.bind(this, '*', '*');
+let textoGuiones = crearCadena.bind(this, '-', '-');
+let textoUnderscore = crearCadena.bind(this, '_', '_');
 
+// No modifiquen nada debajo de esta linea
+// --------------------------------
 
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
-}
+  counter,
+  cacheFunction,
+  getNombreInstructor,
+  getNombreAlumno,
+  textoAsteriscos,
+  textoGuiones,
+  textoUnderscore,
+};
